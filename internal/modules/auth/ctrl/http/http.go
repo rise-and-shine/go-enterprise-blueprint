ef6559b202d58e84777de5d24e6f56042850c66b
Nuriddin Obidjonov <nuriddin.obidjonov777@gmail.com>
2025-11-15 19:29:24 +0500

    ...

--- internal/modules/auth/ctrl/http/http.go
+++ internal/modules/auth/ctrl/http/http.go
@@ -48,54 +48,160 @@ func (s *Server) Stop() error {
 	return s.core.Stop()
 }
 
+// UseCaseRegistry provides a convenient way to register usecases without repetitive code.
+type UseCaseRegistry struct {
+	api             huma.API
+	registrationFns []func()
+}
+
+// NewUseCaseRegistry creates a new usecase registry.
+func NewUseCaseRegistry(api huma.API) *UseCaseRegistry {
+	return &UseCaseRegistry{
+		api:             api,
+		registrationFns: make([]func(), 0),
+	}
+}
+
+// registerOptions holds configuration options for registering usecases.
+type registerOptions struct {
+	Security    openAPISecurity
+	Method      string
+	Path        string
+	OperationID string
+}
+
+// registerOption is a function that modifies RegisterOptions.
+type registerOption func(*registerOptions)
+
+// WithSecurity sets the security configuration.
+func WithSecurity(sec openAPISecurity) registerOption {
+	return func(opts *registerOptions) {
+		opts.Security = sec
+	}
+}
+
+// WithPath sets the HTTP path.
+func WithPath(path string) registerOption {
+	return func(opts *registerOptions) {
+		opts.Path = path
+	}
+}
+
+// AddWrite registers a write usecase with the given API and options.
+func AddWrite[I, O, S any](api huma.API, uc ucdef.UserWriteAction[I, O, S], opts ...registerOption) {
+	// Set default options
+	options := registerOptions{
+		Security:    BearerSecurity(),
+		Method:      http.MethodPost,
+		Path:        uc.OperationID(),
+		OperationID: uc.OperationID(),
+	}
+
+	// Apply provided options
+	for _, opt := range opts {
+		opt(&options)
+	}
+
+	huma.Register(api,
+		huma.Operation{
+			OperationID: options.OperationID,
+			Method:      options.Method,
+			Path:        options.Path,
+			Security:    options.Security,
+		},
+		func(ctx context.Context, i *I) (*O, error) {
+			// Placeholder implementation
+			state, err := uc.Validate(ctx, i)
+			if err != nil {
+				return nil, errx.Wrap(err)
+			}
+
+			auditInfo := uc.AuditInfo(state)
+			logger.Named("auditor").WithContext(ctx).With("audit_info", auditInfo).Info()
+
+			out, err := uc.Execute(ctx, state)
+			return out, errx.Wrap(err)
+		},
+	)
+}
+
+// AddRead registers a read usecase with the given API and options.
+func AddRead[I, O, S any](api huma.API, uc ucdef.UserReadAction[I, O, S], opts ...registerOption) {
+	// Set default options
+	options := registerOptions{
+		Security:    BearerSecurity(),
+		Method:      http.MethodGet,
+		Path:        uc.OperationID(),
+		OperationID: uc.OperationID(),
+	}
+
+	// Apply provided options
+	for _, opt := range opts {
+		opt(&options)
+	}
+
+	huma.Register(api,
+		huma.Operation{
+			OperationID: options.OperationID,
+			Method:      options.Method,
+			Path:        options.Path,
+			Security:    options.Security,
+			Middlewares: huma.Middlewares{
+				func(ctx huma.Context, next func(huma.Context)) {
+					// before
+					next(ctx)
+					// after
+				},
+			},
+		},
+		func(ctx context.Context, i *I) (*O, error) {
+			// Placeholder implementation
+			state, err := uc.Validate(ctx, i)
+			if err != nil {
+				return nil, errx.Wrap(err)
+			}
+
+			// Note: UserReadAction doesn't have AuditInfo method
+			out, err := uc.Execute(ctx, state)
+			return out, errx.Wrap(err)
+		},
+	)
+}
+
 func (s *Server) InitRoutes() {
 	// Here starts Huma duties. huma.rocks
 
 	app := s.core.GetApp()
 	api := humafiber.New(app, huma.DefaultConfig("Go Enterprise Blueprint Auth Service", "v1.0.0"))
 
+	// Add write usecases with the new API
 	uc1001 := uc1001createuser.New(domain.Container{}, service.Container{}, portal.Container{})
+	AddWrite(api, uc1001) // Uses default options: POST, BearerSecurity, operationID from usecase
 
-	ForwardWrite(api, NewWriteHandler(uc1001, BearerSecurity()))
+	// Example with custom options:
+	// AddWrite(api, uc1001,
+	//     WithSecurity(NoSecurity()),
+	//     WithPath("/custom/path"),
+	//     WithMethod(http.MethodPut),
+	// )
+
+	// Add more usecases here:
+	// uc1002 := uc1002updateuser.New(domain.Container{}, service.Container{}, portal.Container{})
+	// AddWrite(api, uc1002, WithSecurity(BearerSecurity()))
+
+	// uc2001 := uc2001getuser.New(domain.Container{}, service.Container{}, portal.Container{})
+	// AddRead(api, uc2001) // Uses default options: GET, BearerSecurity, operationID from usecase
 }
 
-func ForwardWrite[I, O, S any](api huma.API, handler WriteHandler[I, O, S]) {
-	huma.Register(api,
-		huma.Operation{
-			OperationID: handler.UseCase.OperationID(),
-			Method:      http.MethodPost,
-			Path:        handler.UseCase.OperationID(),
-		},
-		Executor(handler.UseCase),
-	)
+type AuthMethod interface {
+	OpenAPISecurity() []map[string][]string
+	AuthMiddleware() []func(ctx huma.Context, next func(huma.Context))
 }
 
-func Executor[I, O, S any](uc ucdef.UserWriteAction[I, O, S]) func(ctx context.Context, i *I) (*O, error) {
-	return func(ctx context.Context, i *I) (*O, error) {
-		state, err := uc.Validate(ctx, i)
-		if err != nil {
-			return nil, errx.Wrap(err)
-		}
-
-		auditInfo := uc.AuditInfo(state)
-		// TODO: produce audit info
-		logger.Named("auditor").WithContext(ctx).With("audit_info", auditInfo).Info()
-
-		out, err := uc.Execute(ctx, state)
-
-		return out, errx.Wrap(err)
-	}
-}
-
-type WriteHandler[I, O, S any] struct {
-	UseCase ucdef.UserWriteAction[I, O, S]
-
-	Security openAPISecurity // default is bearer
-}
-
-func NewWriteHandler[I, O, S any](useCase ucdef.UserWriteAction[I, O, S], sec openAPISecurity) WriteHandler[I, O, S] {
-	return WriteHandler[I, O, S]{useCase, sec}
-}
+const (
+	Bearer AuthMethod = "bearer"
+	NoAuth AuthMethod = "no_auth"
+)
 
 type openAPISecurity []map[string][]string
 
